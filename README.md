eedb519db9020e22321086a22ab8e21d8fe3d55d
juanesgh <31229915+juanesgh@users.noreply.github.com>
2018-03-27 12:22:40 -0300

    Lab 02, parte 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Juan Esteban Rodriguez
+
+La aplicacion no tenia todos los paquetes instalados, por lo tanti lo agregado fue hecho a la antigua, llamese
+crear carpetas y archivos manualmente mediante el explroador de archivos.
---
+++ lab02/app/src/main/java/com/lab/juanrodriguez/lab02/MainActivity.java
@@ -0,0 +1,34 @@
+package com.lab.juanrodriguez.lab02;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+    }
+}
